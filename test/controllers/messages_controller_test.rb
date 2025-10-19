680351d6457f52c078b7e055fff59127a1151623
shinji nakashima <snjx.nksm@gmail.com>
2025-10-19 17:15:02 +0900

    テスト修正

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,13 +1,18 @@
+# test/controllers/messages_controller_test.rb
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get messages_index_url
+    get messages_url   # or messages_path
     assert_response :success
   end
 
-  test "should get create" do
-    get messages_create_url
+  test "should create" do
+    post messages_url, params: { message: { content: "hello", role: "user" } }
+    assert_response :redirect
+    assert_redirected_to root_url
+
+    follow_redirect!
     assert_response :success
   end
 end
